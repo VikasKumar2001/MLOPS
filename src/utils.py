6a6c6f8492d6c7a69ca22716de33a60b7313d14b
Vikas_Kumar <103962309+VikasKumar2001@users.noreply.github.com>
2025-09-27 18:18:13 +0530

    Pipeline Done

--- src/utils.py
+++ src/utils.py
@@ -40,6 +40,15 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
         return report
     except Exception as e:
         raise CustomException(e, sys)
+
+def load_object(filepath):
+    try:
+        with open(filepath, 'rb') as file_obj:
+            return dill.load(file_obj)
+        
+    except Exception as e:
+        raise CustomException(e, sys)
+    
     
 
     
